b9632411ae8ceea50fb515601bafca540fa7678c
xFerchoVJ <fer_elsword@hotmail.com>
2021-10-19 00:24:22 -0500

    First commit

---
+++ src/components/Navegation.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navegation = () => {
+  return (
+    <nav class="nav">
+      <div className="nav__logo">
+        <a href="#" class="nav__logo-link">Portofolio!</a>
+        <h2 class="nav__logo-text">FernandoVJ</h2>
+      </div>
+      <div className="nav__items">
+        <a href="#" class="nav__items-links">About me</a>
+        <a href="#" class="nav__items-links">My experience</a>
+        <a href="#" class="nav__items-links">Contact me</a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navegation
